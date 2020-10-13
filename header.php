6fa4ac3da1c59145462e04afb792fcfca2b4e102
Anthony Chatelot <anthonychxtelxt@gmail.com>
2020-10-13 16:56:49 +0200

    page2

--- header.php
+++ header.php
@@ -3,21 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <title><?=$pagetitle?></title>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Site de révision</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item <?php if ($pagetitle === "Accueil") echo 'active' ?>">
+                    <a class="nav-link" href="index.php">Accueil <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item <?php if ($pagetitle === "page 2") echo 'active' ?>">
+                    <a class="nav-link" href="revision.php">Révisions</a>
+                </li>
+                <li class="nav-item <?php if ($pagetitle === "page 3") echo 'active' ?>">
+                    <a class="nav-link" href="exercice.php">Exercices</a>
+                </li>
+            </ul>
+            
+        </div>
+    </nav>
 
 
-        <ul>
-            <li><a href="revision.php"> Révision</a></li>
-            <li><a href="exercice.php"> Exercices</a></li>
-            <li><a href="index.php"> Accueil</a></li>
-
-        </ul>
-
-    <h1>Révision des tables de multiplication </h1>
-    <p>Ce site vous permettra de réviser les tables de multiplication de 1 à 10 et de vous exercer grace à des exercices.<br></p>
-     
-
+    <h1 class=text-center>Révision des tables de multiplication </h1>
+    <p class=text-center>Ce site vous permettra de réviser les tables de multiplication de 1 à 10 et de vous exercer grace à des exercices.<br></p>
 </body>    
 </html>
--- revision.php
+++ revision.php
@@ -5,41 +5,61 @@ $pagetitle = "Révision tables multiplication";
 require_once "header.php";
 ?>
 
-<form method="GET" action="revision.php">
+<form class=text-center method="POST" action="revision.php">
 Voulez-vous réviser une ou plusieur tables de multiplication ?
 <select name="choix">
     <option value="1">Une seule</option>
     <option value="2">Plusieur</option>
   </select>
-  <button type="submit">Choisir</button>
+  <button type="submit" class="btn btn-primary">Choisir</button>
 
 </form>
 
-<?php if ($_GET['choix']==1): ?>
+
+<?php var_dump($_POST); if ($_POST['choix']==1): ?>
    Choisissez un chiffre entre 1 et 10 :   
-         <form method="GET" action="revision.php">
+         <form method="POST" action="revision.php">
              <input type="number" name="chiffre" placeholder="entre 1 et 10">
-             <button type="submit">Choisir</button>
+
+             <button type="submit" class="btn btn-primary">Choisir</button>
           </form>
 
-
-<?php elseif ($_GET['choix']==2): ?>
+<?php elseif ($_POST['choix']==2): ?>
     Choisissez les tables que vous voulez réviser :
 
-    <form method="GET" action="revision.php">
-             <input type="number" name="chiffre" placeholder="entre 1 et 10">
-    </form>
-    <form method="GET" action="revision.php">
-             <input type="number" name="chiffre" placeholder="entre 1 et 10">
-    </form>
-    <form method="GET" action="revision.php">
-             <input type="number" name="chiffre" placeholder="entre 1 et 10">
-    </form>
-    <form method="GET" action="revision.php">
-             <input type="number" name="chiffre" placeholder="entre 1 et 10">
-    </form>
+    
+    
+    <?php for ($i = 1; $i <= 10; $i++) : ?>
 
-    <button type="submit">Choisir</button>
+<div class="form-check form-check-inline">
 
+    <input class="form-check-input" name="check<?= $i ?>" type="checkbox" value="<?= $i ?>" id="check<?= $i ?>" <?php if (!empty($_POST) && isset($_POST["check$i"]) && $i === intval($_POST["check$i"])) echo "checked"; ?>>
+
+    <label class="form-check-label" for="check<?= $i ?>">
+        <?= $i ?>
+    </label>
+</div>
+
+<?php endfor; ?>
+<button type="submit" class="btn btn-primary">Choisir !</button>
+</div>
+</form>
 
 <?php endif ?>
+
+
+<?php if ($_POST['chiffre'] <=10 ): ?> 
+  <?php $i = $_POST['chiffre'] ; 
+       
+     Echo 'Table de multiplication de '.$i.'<br>';
+     for ($j = 1; $j <= 10; $j++)
+     {
+         echo $i.' x '.$j.' = '.$i*$j.'<br>';
+     }
+     ?>
+
+    <?php elseif ($_POST['chiffre']>10): ?>
+    Chiffre trop grand! Choisissez un chiffre entre 1 et 10
+      
+    <?php endif ?>
+                
